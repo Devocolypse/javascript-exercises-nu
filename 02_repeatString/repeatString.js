96750c08f29bbb6dbe80ae31b921fdcab41aaeb5
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-15 12:33:14 -0700

    feat: Add passing code for string repeater
    It is worth noting that TOP's solution, while practically the same,
    wins out in brevity by using a one-line IF statement instead of my
    IF-ELSE statement.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        let echo = '';
+        
+        for (let i = 0; i < num; i++) {
+            echo += string;
+        }
+        return echo;
+    }
 };
 
 // Do not edit below this line
