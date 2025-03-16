1f33ec3722883cf1fd08400b25a34f430bb32baa
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-16 01:00:10 -0700

    feat: Pass all tests and with a while loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    // checks for "correct" value types first
+    if (
+        x < 0 ||
+        y < 0 ||
+        Math.floor(x) !== x ||
+        Math.floor(y) !== y
+    ) return 'ERROR';
 
+    let finalSum = 0;
+    let start = x;
+    let end = y;
+
+    // ensures incrementive loop works regardless of whether 'x' or 'y' is larger
+    if (x > y) {
+        start = y;
+        end = x;
+    }
+    
+    // after each addition, the gap between the original 'x' and 'y' shortens
+    while (start <= end) {
+        finalSum += start;
+        start++;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
