ec1e18a967726e945aa90c9120c860130e649099
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-16 00:00:47 -0700

    feat: Make array removal work with a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,21 @@
 const removeFromArray = function(original, ...blackList) {
     // create a new array variable to hold the final result
+    let filtered = [];
+    
     // FOR each item in the array, check to see if is on the blacklist
-        // FOR each item on the blacklist (arbitrary), check if they're equal
-            // IF so, then CONTINUE to the next item in the original array
+    OUTER: for (item of original) {
+        // FOR each item on the blacklist (arbitrary)
+        for (blackItem of blackList) {
+            /* check if they're equal IF so, then CONTINUE 
+            to the next item in the original array */
+            if (item === blackItem) continue OUTER;
             // ELSE, add the item to the new array
+            filtered.push(item)
+        }
+    }
+
     // return the new array
+    return filtered;
 };
 
 // Do not edit below this line
