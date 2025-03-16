629ef4a43e5f693810d0d26a3960371920dd519a
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-16 01:21:03 -0700

    feat: Pass tests and add explanation of leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // for the definition of a leap year, please refer to:
+    // https://time-and-calendar.com/leap-years/
 
+    if (
+        year % 4 === 0 &&
+        year % 100 !== 0
+    ) {
+        return true;
+    } else if (
+        year % 4 === 0 && 
+        year % 100 === 0 && 
+        year % 400 === 0
+    ) {
+        return true
+    }
+
+    return false;
 };
 
 // Do not edit below this line
