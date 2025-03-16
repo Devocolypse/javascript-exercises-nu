a5e165931de0e5a41a046bb5cdd24d1dc76e15ca
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-16 01:41:05 -0700

    feat: Add functionality for Celsius --> Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -5,6 +5,9 @@ const convertToCelsius = function(tempInFah) {
 };
 
 const convertToFahrenheit = function(tempInCel) {
+  const formula = tempInCel * (9 / 5) + 32;
+  const rounded = parseFloat(formula.toFixed(1));
+  return rounded;
 };
 
 // Do not edit below this line
