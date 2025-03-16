687e4db0f535fa65183d49e697fedfc3a4211467
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-15 23:27:03 -0700

    feat: Add method to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(original) {
+    let reversed = "";
 
+    for (let i = original.length; i > 0; i--) {
+        let currentChar = original.at(i - 1);
+        reversed += currentChar;
+    }
+    
+    return reversed;
 };
+
+reverseString("epic")
 
 // Do not edit below this line
 module.exports = reverseString;
