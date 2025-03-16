4ce54397c9ab5ea2c653403a4a7a4330a576edab
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-16 00:13:35 -0700

    docs: Remove over-obvious pseudocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,14 @@
+// Filters out arbitrary elements from an array non-destructively
 const removeFromArray = function(original, ...blackList) {
-    // create a new array variable to hold the final result
     let filtered = [];
     
-    // FOR each item in the array, check to see if is on the blacklist
     OUTER: for (item of original) {
-        // FOR each item on the blacklist (arbitrary)
         for (blackItem of blackList) {
-            /* check if they're equal IF so, then CONTINUE 
-            to the next item in the original array */
             if (item === blackItem) continue OUTER;
         }
-        // ELSE, add the item to the new array
         filtered.push(item)
     }
 
-    // return the new array
     return filtered;
 };
 
