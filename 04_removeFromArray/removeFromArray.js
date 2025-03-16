5a6469658b2c46e863f7e0e89c73d06c99af0b0c
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-15 23:53:01 -0700

    feat: Add pseudocode for preservatory method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(original, ...blackList) {
+    // create a new array variable to hold the final result
+    // FOR each item in the array, check to see if is on the blacklist
+        // FOR each item on the blacklist (arbitrary), check if they're equal
+            // IF so, then CONTINUE to the next item in the original array
+            // ELSE, add the item to the new array
+    // return the new array
 };
 
 // Do not edit below this line
